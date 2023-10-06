e36875f8a34fcdf08bf43f3c87565f31e59ea360
rasyadpras <106673670+rasyadpras@users.noreply.github.com>
2023-10-06 23:35:00 +0700

    fixing error features

--- banking_system.js
+++ banking_system.js
@@ -4,41 +4,61 @@ class BankAccount {
         this.show = document.getElementById("show").textContent;
     }
 
-    deposit(amount) {
-        let amount = prompt("Masukkan Nilai");
+    async deposit(amount) {
         let balance = parseFloat(amount);
         if (isNaN(balance)) {
-            throw Error('Input invalid. Mohon masukkan angka yang benar.');
+            alert('Input invalid. Mohon masukkan angka yang benar.');
         } else {
-            if (balance < 0) {
-                throw Error('Nilai harus lebih dari 0');
+            if (balance < 1000) {
+                throw Error('Nilai harus lebih dari 1000');
             } else {
-                setTimeout(() => this.saldo += balance, 1000); //bikin class sendiri
-                console.log(`Setoran diterima.\nSaldo anda saat ini : ${this.saldo}`);
+                setTimeout(() => {
+                    this.saldo += balance;
+                    console.log(this.saldo);
+                    this.show = this.saldo;
+                    alert(`Setoran diterima.\nSaldo anda saat ini : ${this.saldo}`);
+                }, 1000);
             }
         }
     }
 
-    withdraw(amount) {
-        let amount = prompt("Masukkan Nilai");
+    async withdraw(amount) {
         let balance = parseFloat(amount);
         if (isNaN(balance)) {
-            throw Error('Input invalid. Mohon masukkan angka yang benar.');
+            alert('Input invalid. Mohon masukkan angka yang benar.');
         } else {
             if (balance < 0) {
                 throw Error('Nilai harus lebih dari 0');
             } else {
-                setTimeout(() => this.saldo -= balance, 1000);
-                if (this.saldo < 0) {
-                    throw Error('Penarikan gagal. Saldo kurang dari 0');
-                } else {
-                    console.log(`Penarikan berhasil.\nSaldo anda saat ini : ${this.saldo}`);
-                }
+                setTimeout(() => {
+                    this.saldo -= balance;
+                    if (this.saldo < 0) {
+                        throw Error('Penarikan gagal. Saldo kurang dari 0');
+                    } else {
+                        console.log(this.saldo);
+                        this.show = this.saldo;
+                        alert(`Penarikan berhasil.\nSaldo anda saat ini : ${this.saldo}`);
+                    }
+                }, 1000);
+                
             }
         }
     }
-
-
 };
 
+// inheritance
+class TransactionButton extends BankAccount {
+
+    deposit() {
+        let amount = prompt("Masukkan jumlah uang yang ingin anda tabung");
+        super.deposit(amount);
+    }
+
+    withdraw() {
+        let amount = prompt("Masukkan jumlah uang yang ingin anda tarik");
+        super.withdraw(amount);
+    }
+}
+
 const bankAccount = new BankAccount;
+const button = new TransactionButton;
